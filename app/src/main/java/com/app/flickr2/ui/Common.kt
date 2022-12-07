a1d035a404168efa2bf2edf13427ce3b2239ec29
Igor Novikov <igor.novikov@umbrellait.com>
2022-12-07 22:57:02 +0300

    search screen savepoint

--- app/src/main/java/com/app/flickr2/ui/Common.kt
+++ app/src/main/java/com/app/flickr2/ui/Common.kt
@@ -2,13 +2,16 @@ package com.app.flickr2.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
